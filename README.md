b0323016aab6750c7ef00ec0c1273a817771564d
wout <wout@impinc.co.uk>
2013-02-02 19:08:45 +0100

    Added basic import

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-svg.import.js
-=============
+# svg.import.js
 
-A plugin for importing raw SVG into the svg.js library
+This is a plugin for the [svg.js](http://svgjs.com) library to import raw SVG data.
+
+Svg.import.js is licensed under the terms of the MIT License.
+
+
+## Usage
+
+Include this plugin after including svg.js in your html document.
+
+```javascript
+var rawSvg = '<?xml version="1.0" encoding="utf-8"?><!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 width="500px" height="500px" viewBox="0 0 500 500" enable-background="new 0 0 500 500" xml:space="preserve"><line fill="none" stroke="#FF7BAC" stroke-width="20" stroke-linecap="round" stroke-miterlimit="10" x1="138.682" y1="250" x2="293.248" y2="95.433"/><rect x="22.48" y="19.078" fill="#F7931E" stroke="#C1272D" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" width="94.972" height="94.972"/><path opacity="0.5" fill="#29ABE2" d="M189.519,131.983c0,5.523-4.477,10-10,10H92.257c-5.523,0-10-4.477-10-10V53.659	c0-5.523,4.477-10,10-10h87.262c5.523,0,10,4.477,10,10V131.983z"/><circle opacity="0.8" fill="#8CC63F" cx="201.603" cy="159.508" r="69.067"/><polygon fill="none" stroke="#8C6239" stroke-width="20" stroke-linecap="round" stroke-miterlimit="10" points="286.331,287.025 	227.883,271.365 212.221,212.915 255.009,170.127 313.459,185.789 329.119,244.237 "/></svg>';
+
+var draw = svg('paper');
+
+draw.import(rawSvg);
+```
+
+## Important
+This plugin is still under development and will be improved in the coming months.
+Please report issues and suggestions are welcome too.
+
+## To-do
+- Importing text, defs, gradients and patterns
---
+++ svg.import.js
@@ -0,0 +1,86 @@
+// svg.import.js 0.1 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
+
+SVG.extend(SVG.Container, {
+  // Add import method to container elements
+  import: function(raw) {
+    /* create temporary div to receive svg content */
+    var well = document.createElement('div');
+    
+    /* properly close svg tags and add them to the DOM */
+    well.innerHTML = raw.replace(/<(\w+)([^<]+?)\/>/g, '<$1$2></$1>');
+    
+    /* convert nodes to svg elements */
+    this._convertNodes(well.childNodes, this, 0);
+    
+    /* mark temporary div for garbage collection */
+    well = null;
+    
+    return this;
+  }
+  // Convert nodes to svg.js elements
+, _convertNodes: function(nodes, context, level) {
+    var i, l, n, key, attrs;
+    
+    for (i = 0, l = nodes.length; i < l; i++) {
+      var element, type
+        , child = nodes[i]
+        , attr  = {};
+      
+      /* get node type */
+      type = child.nodeName.toLowerCase();
+      
+      if (type != '#comment') {
+        /*  objectify attributes */
+        attrs = [].slice.call(child.attributes);
+        for (n = attrs.length - 1; n >= 0; n--)
+          attr[attrs[n].nodeName] = attrs[n].nodeValue;
+
+        /* create elements */
+        switch(type) {
+          case 'rect':
+          case 'circle':
+          case 'ellipse':
+            element = context[type](0,0);
+          break;
+          case 'line':
+            element = context.line(0,0,0,0);
+          break;
+          case 'text':
+          case 'path':
+          case 'polygon':
+          case 'polyline':
+            element = context[type]();
+          break;
+          case 'image':
+            element = context.image(attr['xlink:href']);
+          break;
+          case 'g':
+          case 'svg':
+            if (type == 'svg' && level == 0) {
+              this._convertNodes(child.childNodes, context, level + 1);
+              return context;
+            } else {
+              element = context[type == 'g' ? 'group' : 'nested']();
+              this._convertNodes(child.childNodes, element, level + 1);
+            }
+          break;
+        };
+
+        /* parse attributes */
+        switch(type) {
+          case 'circle':
+            attr.rx = attr.r;
+            attr.ry = attr.r;
+            delete attr.r;
+          break;
+        };
+
+        /* set attributes */
+        element.attr(attr);
+      }
+    };
+    
+    return context;
+  }
+  
+});
