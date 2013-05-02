4b7f752150bc15143b19e72ed291dff29b7f23fe
John Harding <john@memloom.com>
2013-05-02 02:50:26 -0400

    Added text / tspan & def / clippath importing

--- svg.import.js
+++ svg.import.js
@@ -19,24 +19,31 @@ SVG.extend(SVG.Container, {
     
     return store
   }
+
+, _objectifyAttributes: function(child) {
+    var attrs, attr = {}
+    attrs = child.attributes || []    
+    for (n = attrs.length - 1; n >= 0; n--)
+      attr[attrs[n].nodeName] = attrs[n].nodeValue
+    return attr
+  }
+
   // Convert nodes to svg.js elements
 , _convertNodes: function(nodes, context, level, store, block) {
-    var i, l, n, key, attrs
+    var i, l, n, key
     
     for (i = 0, l = nodes.length; i < l; i++) {
       var element, type
         , child = nodes[i]
         , attr  = {}
+        , clips = []
       
       /* get node type */
       type = child.nodeName.toLowerCase()
       
       /*  objectify attributes */
-      attrs = child.attributes || []
-      
-      for (n = attrs.length - 1; n >= 0; n--)
-        attr[attrs[n].nodeName] = attrs[n].nodeValue
-      
+      attr = this._objectifyAttributes(child)
+     
       /* create elements */
       switch(type) {
         case 'rect':
@@ -48,6 +55,22 @@ SVG.extend(SVG.Container, {
           element = context.line(0,0,0,0)
         break
         case 'text':
+          if (child.childNodes.length == 0) {
+            element = context[type](child.textContent)
+          }
+          else {
+            element = null
+            for (var j=0;j<child.childNodes.length;j++) {
+              var grandchild = child.childNodes[j]
+              if (grandchild.nodeName.toLowerCase() == 'tspan') {
+                if (element === null) // first time through call the text() function on the current context
+                  element = context[type](grandchild.textContent)
+                else // for the remaining times create additional tspans
+                  element.tspan(grandchild.textContent).attr(this._objectifyAttributes(grandchild))
+              }
+            }
+          }
+        break
         case 'path':
         case 'polygon':
         case 'polyline':
@@ -61,6 +84,23 @@ SVG.extend(SVG.Container, {
           element = context[type == 'g' ? 'group' : 'nested']()
           this._convertNodes(child.childNodes, element, level + 1, store, block)
         break
+        case 'defs':
+          for (var j=0;j<child.childNodes.length;j++) {
+            var grandchild = child.childNodes[j]
+            if (grandchild.nodeName.toLowerCase() == 'clippath') {
+              var clip = (this.defs().put(new SVG.Clip)).attr(this._objectifyAttributes(grandchild))
+              this._convertNodes(grandchild.childNodes, clip, level + 1, store, block)
+            }
+          }
+        break
+        case '#comment':
+        case '#text':
+        case 'metadata':
+        case 'desc':
+          // safely ignore these elements
+        break
+        default:
+          console.log("SVG Import got unexpected type " + type, child)
       }
       
       /* parse attributes */
@@ -77,7 +117,11 @@ SVG.extend(SVG.Container, {
         element.attr(attr)
         if (element.attr('id'))
           store[element.attr('id')] = element
-        
+
+        // now that we've set the attributes "rebuild" the text to correctly set the attributes 
+        if (type == 'text')
+          element.rebuild()
+
         /* call block if given */
         if (typeof block == 'function')
           block.call(element)
