d86c83aab558ac6a1c932cc67685318576cdda74
wout <wout@impinc.co.uk>
2013-06-27 18:09:19 +0100

    Parsing transformations, bumped to v0.8

--- spec/spec/svg.import.js
+++ spec/spec/svg.import.js
@@ -6,12 +6,12 @@ describe('Import', function() {
   
   describe('svg()', function() {
     
-    it('should store imported element in an object', function() {
+    it('stores imported element in an object', function() {
       var store = draw.svg(rawSVG)
       expect(typeof store).toBe('object')
     })
     
-    it('should make imported elements accessible in store with their ID', function() {
+    it('makes imported elements accessible in store with their ID', function() {
       var store = draw.svg(rawSVG)
       expect(!!store.line1234).toBe(true)
       expect(!!store.rect1235).toBe(true)
@@ -20,7 +20,7 @@ describe('Import', function() {
       expect(!!store.polygon1238).toBe(true)
     })
     
-    it('should typecast imported elements correctly', function() {
+    it('typecasts imported elements correctly', function() {
       var store = draw.svg(rawSVG)
       expect(store.line1234 instanceof SVG.Line).toBe(true)
       expect(store.rect1235 instanceof SVG.Rect).toBe(true)
@@ -29,14 +29,63 @@ describe('Import', function() {
       expect(store.polygon1238 instanceof SVG.Polygon).toBe(true)
     })
     
-    it('should apply a passed block to every element in the structure', function() {
+    it('applies a passed block to every element in the structure', function() {
       var expected = ('line1234 rect1235 path1236 circle1237 polygon1238 Layer_1').split(' ')
         , ids = []
         , store = draw.svg(rawSVG, function() { ids.push(this.attr('id')) })
       
       expect(ids).toEqual(expected)
     })
+
+    describe('parses transformation', function() {
+      var store, poly
+
+      beforeEach(function() {
+        store = draw.svg(rawSVG)
+        poly = SVG.get('polygon1238')
+      })
+
+      it('attribute', function() {
+        var circle = SVG.get('circle1237')
+
+        expect(circle.attr('transform')).toBe('rotate(45,80,120)')
+        expect(circle.transform('rotation')).toBe(45)
+      })
+      it('rotate and rotation point', function() {
+        expect(poly.transform('rotation')).toBe(45)
+        expect(poly.transform('cx')).toBe(80)
+        expect(poly.transform('cy')).toBe(120)
+      })
+      it('matrix', function() {
+        expect(poly.transform('a')).toBe(-0.5)
+        expect(poly.transform('b')).toBe(0)
+        expect(poly.transform('c')).toBe(0)
+        expect(poly.transform('d')).toBe(1.5)
+        expect(poly.transform('e')).toBe(0)
+        expect(poly.transform('f')).toBe(1)
+      })
+      it('scale', function() {
+        expect(poly.transform('scaleX')).toBe(0.3)
+        expect(poly.transform('scaleY')).toBe(0.5)
+      })
+      it('skewX', function() {
+        expect(poly.transform('skewX')).toBe(-30)
+      })
+      it('skewY', function() {
+        expect(poly.transform('skewY')).toBe(99.99999)
+      })
+      it('translate', function() {
+        expect(poly.transform('x')).toBe(-10)
+        expect(poly.transform('y')).toBe(655)
+      })
+    })
     
   })
   
 })
+
+
+
+
+
+
--- svg.import.js
+++ svg.import.js
@@ -1,40 +1,10 @@
-// svg.import.js 0.7 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
-SVG.extend(SVG.Container, {
-  // Add import method to container elements
-  svg: function(raw, block) {
-    /* create temporary div to receive svg content */
-    var well = document.createElement('div')
-      , store = {}
-    
-    /* properly close svg tags and add them to the DOM */
-    well.innerHTML = raw
-      .replace(/\n/, '')
-      .replace(/<(\w+)([^<]+?)\/>/g, '<$1$2></$1>')
-    
-    /* convert nodes to svg elements */
-    this._convertNodes(well.childNodes, this, 0, store, block)
-    
-    /* mark temporary div for garbage collection */
-    well = null
-    
-    return store
-  }
+// svg.import.js 0.8 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
+(function() {
+  var convertNodes, objectifyAttributes, objectifyTransformations
 
-  // Private: convert attributes to an object
-, _objectifyAttributes: function(child) {
-    var n
-      , attrs = child.attributes || []
-      , attr  = {}
-
-    for (n = attrs.length - 1; n >= 0; n--)
-      attr[attrs[n].nodeName] = attrs[n].nodeValue
-
-    return attr
-  }
-
-  // Private: convert nodes to svg.js elements
-, _convertNodes: function(nodes, context, level, store, block) {
-    var i, l, j, key, element, type, child, attr, clips
+  // Convert nodes to svg.js elements
+  convertNodes = function(nodes, context, level, store, block) {
+    var i, l, j, key, element, type, child, attr, transform, clips
     
     for (i = 0, l = nodes.length; i < l; i++) {
       child = nodes[i]
@@ -45,7 +15,7 @@ SVG.extend(SVG.Container, {
       type = child.nodeName
       
       /*  objectify attributes */
-      attr = this._objectifyAttributes(child)
+      attr = objectifyAttributes(child)
      
       /* create elements */
       switch(type) {
@@ -60,28 +30,28 @@ SVG.extend(SVG.Container, {
         case 'text':
           if (child.childNodes.length == 0) {
             element = context[type](child.textContent)
-
+  
           } else {
             var grandchild
-
+  
             element = null
-
+  
             for (j = 0; j < child.childNodes.length; j++) {
               grandchild = child.childNodes[j]
-
+  
               if (grandchild.nodeName.toLowerCase() == 'tspan') {
                 if (element === null)
                   /* first time through call the text() function on the current context */
                   element = context[type](grandchild.textContent)
-
+  
                 else
                   /* for the remaining times create additional tspans */
                   element
                     .tspan(grandchild.textContent)
-                    .attr(this._objectifyAttributes(grandchild))
+                    .attr(objectifyAttributes(grandchild))
               }
             }
-
+  
           }
         break
         case 'path':
@@ -95,36 +65,22 @@ SVG.extend(SVG.Container, {
         case 'g':
         case 'svg':
           element = context[type == 'g' ? 'group' : 'nested']()
-          this._convertNodes(child.childNodes, element, level + 1, store, block)
+          convertNodes(child.childNodes, element, level + 1, store, block)
         break
         case 'defs':
-          //-D  var j, grandchild, node, name
-          //-D  for (j = 0; j < child.childNodes.length; j++) {
-          //-D    grandchild = child.childNodes[j]
-          //-D    name = grandchild.nodeName
-          //-D    if (name == 'clipPath' || name == 'mask') {
-          //-D      node = this.defs()
-          //-D        .put(name == 'mask' ? new SVG.Mask : new SVG.Clip)
-          //-D        .attr(this._objectifyAttributes(grandchild))
-          //-D      this._convertNodes(grandchild.childNodes, node, level + 1, store, block)
-          //-D    }
-          //-D  }
-          this._convertNodes(child.childNodes, context.defs(), level + 1, store, block)
+          convertNodes(child.childNodes, context.defs(), level + 1, store, block)
         break
         case 'clipPath':
         case 'mask':
           element = context[type == 'mask' ? 'mask' : clip]()
-          this._convertNodes(child.childNodes, element, level + 1, store, block)
+          convertNodes(child.childNodes, element, level + 1, store, block)
         break
         case 'linearGradient':
         case 'radialGradient':
-          var self = this
-
           element = context.defs().gradient(type.split('Gradient')[0], function(stop) {
             for (var j = 0; j < child.childNodes.length; j++) {
-              console.log(child)
               stop
-                .at(self._objectifyAttributes(child.childNodes[j]))
+                .at(objectifyAttributes(child.childNodes[j]))
                 .style(child.childNodes[j].getAttribute('style'))
             }
           })
@@ -150,17 +106,23 @@ SVG.extend(SVG.Container, {
       }
       
       if (element) {
-        /* set attributes */
-        element.attr(attr)
+        /* parse transform attribute */
+        transform = objectifyTransformations(attr.transform)
+        delete attr.transform
 
+        /* set attributes and transformations */
+        element
+          .attr(attr)
+          .transform(transform)
+  
         /* store element by id */
         if (element.attr('id'))
           store[element.attr('id')] = element
-
+  
         /* now that we've set the attributes "rebuild" the text to correctly set the attributes */
         if (type == 'text')
           element.rebuild()
-
+  
         /* call block if given */
         if (typeof block == 'function')
           block.call(element)
@@ -169,5 +131,89 @@ SVG.extend(SVG.Container, {
     
     return context
   }
+
+  // Convert attributes to an object
+  objectifyAttributes = function(child) {
+    var i
+      , attrs = child.attributes || []
+      , attr  = {}
+    
+    /* gether attributes */
+    for (i = attrs.length - 1; i >= 0; i--)
+      attr[attrs[i].nodeName] = attrs[i].nodeValue
   
-})
+    return attr
+  }
+
+  // Convert transformations to an object
+  objectifyTransformations = function(transform) {
+    var i, t, v
+      , trans = {}
+      , list  = (transform || '').match(/[A-Za-z]+\([^\)]+\)/g) || []
+      , def   = SVG.defaults.trans()
+
+    /* gather transformations */
+    for (i = list.length - 1; i >= 0; i--) {
+      /* parse transformation */
+      t = list[i].match(/([A-Za-z]+)\(([^\)]+)\)/)
+      v = (t[2] || '').replace(/\s+/g, '').split(',')
+
+      /* objectify transformation */
+      switch(t[1]) {
+        case 'matrix':
+          trans.a         = parseFloat(v[0]) || def.a
+          trans.b         = parseFloat(v[1]) || def.b
+          trans.c         = parseFloat(v[2]) || def.c
+          trans.d         = parseFloat(v[3]) || def.d
+          trans.e         = parseFloat(v[4]) || def.e
+          trans.f         = parseFloat(v[5]) || def.f
+        break
+        case 'rotate':
+          trans.rotation  = parseFloat(v[0]) || def.rotation
+          trans.cx        = parseFloat(v[1]) || def.cx
+          trans.cy        = parseFloat(v[2]) || def.cy
+        break
+        case 'scale':
+          trans.scaleX    = parseFloat(v[0]) || def.scaleX
+          trans.scaleY    = parseFloat(v[1]) || def.scaleY
+        break
+        case 'skewX':
+          trans.skewX     = parseFloat(v[0]) || def.skewX
+        break
+        case 'skewY':
+          trans.skewY     = parseFloat(v[0]) || def.skewY
+        break
+        case 'translate':
+          trans.x         = parseFloat(v[0]) || def.x
+          trans.y         = parseFloat(v[1]) || def.y
+        break
+      }
+    }
+
+    return trans
+  }
+
+  SVG.extend(SVG.Container, {
+    // Add import method to container elements
+    svg: function(raw, block) {
+      /* create temporary div to receive svg content */
+      var well = document.createElement('div')
+        , store = {}
+      
+      /* properly close svg tags and add them to the DOM */
+      well.innerHTML = raw
+        .replace(/\n/, '')
+        .replace(/<(\w+)([^<]+?)\/>/g, '<$1$2></$1>')
+      
+      /* convert nodes to svg elements */
+      convertNodes(well.childNodes, this, 0, store, block)
+      
+      /* mark temporary div for garbage collection */
+      well = null
+      
+      return store
+    }
+    
+  })
+
+}).call(this)
