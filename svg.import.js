4e5f2d5c92a5fc9de2eedaa3acac02e24bcaea06
wout <wout@impinc.co.uk>
2014-02-04 18:16:32 +0100

    Bumped to 1.0.0

--- svg.import.js
+++ svg.import.js
@@ -1,9 +1,8 @@
-// svg.import.js 0.16 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
+// svg.import.js 1.0.0 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
 ;(function() {
-  var convertNodes, objectifyAttributes, objectifyTransformations
 
   // Convert nodes to svg.js elements
-  convertNodes = function(nodes, context, level, store, block) {
+  function convertNodes(nodes, context, level, store, block) {
     var i, l, j, key, element, type, child, attr, transform, clips
     
     for (i = 0, l = nodes.length; i < l; i++) {
@@ -15,7 +14,7 @@
       type = child.nodeName.toLowerCase()
       
       /*  objectify attributes */
-      attr = objectifyAttributes(child)
+      attr = SVG.parse.attr(child)
      
       /* create elements */
       switch(type) {
@@ -48,14 +47,14 @@
                   /* for the remaining times create additional tspans */
                   element
                     .tspan(grandchild.textContent)
-                    .attr(objectifyAttributes(grandchild))
+                    .attr(SVG.parse.attr(grandchild))
               }
             }
   
           }
         break
         case 'path':
-          element = context.path(attr['d'], true)
+          element = context.path(attr['d'])
         break
         case 'polygon':
         case 'polyline':
@@ -86,7 +85,7 @@
             for (var j = 0; j < child.childNodes.length; j++) {
               stop
                 .at({ offset: 0 })
-                .attr(objectifyAttributes(child.childNodes[j]))
+                .attr(SVG.parse.attr(child.childNodes[j]))
                 .style(child.childNodes[j].getAttribute('style'))
             }
           })
@@ -113,7 +112,7 @@
       
       if (element) {
         /* parse transform attribute */
-        transform = objectifyTransformations(attr.transform)
+        transform = SVG.parse.transform(attr.transform)
         delete attr.transform
 
         /* set attributes and transformations */
@@ -136,71 +135,6 @@
     }
     
     return context
-  }
-
-  // Convert attributes to an object
-  objectifyAttributes = function(child) {
-    var i
-      , attrs = child.attributes || []
-      , attr  = {}
-    
-    /* gather attributes */
-    for (i = attrs.length - 1; i >= 0; i--)
-      attr[attrs[i].nodeName] = attrs[i].nodeValue
-
-    /* ensure stroke width where needed */
-    if (typeof attr.stroke != 'undefined' && typeof attr['stroke-width'] == 'undefined')
-      attr['stroke-width'] = 1
-  
-    return attr
-  }
-
-  // Convert transformations to an object
-  objectifyTransformations = function(transform) {
-    var i, t, v
-      , trans = {}
-      , list  = (transform || '').match(/[A-Za-z]+\([^\)]+\)/g) || []
-      , def   = SVG.defaults.trans()
-
-    /* gather transformations */
-    for (i = list.length - 1; i >= 0; i--) {
-      /* parse transformation */
-      t = list[i].match(/([A-Za-z]+)\(([^\)]+)\)/)
-      v = (t[2] || '').replace(/^\s+/,'').replace(/,/g, ' ').replace(/\s+/g, ' ').split(' ')
-
-      /* objectify transformation */
-      switch(t[1]) {
-        case 'matrix':
-          trans.a         = SVG.regex.isNumber.test(v[0]) ? parseFloat(v[0]) : def.a
-          trans.b         = parseFloat(v[1]) || def.b
-          trans.c         = parseFloat(v[2]) || def.c
-          trans.d         = SVG.regex.isNumber.test(v[3]) ? parseFloat(v[3]) : def.d
-          trans.e         = parseFloat(v[4]) || def.e
-          trans.f         = parseFloat(v[5]) || def.f
-        break
-        case 'rotate':
-          trans.rotation  = parseFloat(v[0]) || def.rotation
-          trans.cx        = parseFloat(v[1]) || def.cx
-          trans.cy        = parseFloat(v[2]) || def.cy
-        break
-        case 'scale':
-          trans.scaleX    = SVG.regex.isNumber.test(v[0]) ? parseFloat(v[0]) : def.scaleX
-          trans.scaleY    = SVG.regex.isNumber.test(v[1]) ? parseFloat(v[1]) : def.scaleY
-        break
-        case 'skewX':
-          trans.skewX     = parseFloat(v[0]) || def.skewX
-        break
-        case 'skewY':
-          trans.skewY     = parseFloat(v[0]) || def.skewY
-        break
-        case 'translate':
-          trans.x         = parseFloat(v[0]) || def.x
-          trans.y         = parseFloat(v[1]) || def.y
-        break
-      }
-    }
-
-    return trans
   }
 
   SVG.extend(SVG.Container, {
@@ -233,9 +167,6 @@
 
   SVG.extend(SVG.ImportStore, {
     add: function(key, element, root) {
-      /* DEPRECATED old method of storing elements in the store object */
-      this[key] = element
-
       /* store element in local store object */
       if (key) {
         if (this._importStore[key]) {
